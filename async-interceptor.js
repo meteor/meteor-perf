bbfcdf21e507b91968f9fc72746e9fce2941a687
Leonardo Venturini <leovenbag@gmail.com>
2024-10-16 08:05:46 -0400

    add garbage collection to async resource map

--- async-interceptor.js
+++ async-interceptor.js
@@ -1,25 +1,39 @@
 import async_hooks from 'node:async_hooks';
 
-export const asyncResources = new Map();
+// 50% of map limit
+export const GC_LIMIT = Math.pow(2, 23);
+
+export const AsyncResourceMap = new Map();
 
 export const AsyncInterceptor = async_hooks.createHook({
-  init(asyncId, type, triggerAsyncId, resource) {
-    logResourceCreation(asyncId, type, triggerAsyncId, resource);
+  init(asyncId, type) {
+    logResourceCreation(asyncId, type);
   },
 });
 
-function logResourceCreation(asyncId, type, triggerAsyncId, resource) {
+function logResourceCreation(asyncId, type) {
   const stack = (new Error()).stack.split('\n').slice(2).filter(line => {
     return !['AsyncHook.init', 'node:internal/async_hooks'].some(fn => line.includes(fn));
   }).join('\n');
 
-  if (!asyncResources.has(stack)) {
-    asyncResources.set(stack, { count: 0, types: new Set() });
+  if (AsyncResourceMap.size > GC_LIMIT) {
+    console.log('Meteor Perf: Garbage collecting async resources');
+    garbageCollectAsyncResources();
   }
 
-  const resourceInfo = asyncResources.get(stack);
+  if (!AsyncResourceMap.has(stack)) {
+    AsyncResourceMap.set(stack, { count: 0, types: new Set() });
+  }
+
+  const resourceInfo = AsyncResourceMap.get(stack);
   resourceInfo.count++;
   resourceInfo.types.add(type);
 }
 
-
+function garbageCollectAsyncResources() {
+  AsyncResourceMap.forEach((info, stack) => {
+    if (info.count <= 1) {
+      AsyncResourceMap.delete(stack);
+    }
+  });
+}
